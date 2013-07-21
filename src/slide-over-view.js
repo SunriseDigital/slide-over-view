8f1becf1660a3b1c8752b25a89d110ca5ab228b5
Masamoto Miyata <miyata@sincere-co.com>
2013-07-21 14:22:12 +0900

    CSSのclassを使った方法を試す。

--- src/slide-over-view.js
+++ src/slide-over-view.js
@@ -1,48 +1,63 @@
 ;(function($){
 "use strict";
 
+function disableScroll(){
+	var body = $('body');
+
+	body.css('overflow', 'hidden');
+
+	body.bind('touchmove', function(e){
+		e.preventDefault();
+		return false;
+	});
+}
+
+function enableScroll(){
+	var body = $('body');
+
+	body.css('overflow', 'auto');
+
+	body.unbind('touchmove');
+}
+
 $.fn.extend({
     slideOverView: function(options){
 		return this.each(function(){
 			var view = $(this);
 			var op = $.extend({}, options);
-			var duration = '700ms';
 
-			var wrap = $('<div class="slideOverView"></div>')
+			var wrap = $('<div class="sovWrapper sovHide sovNoTransition"></div>')
 				.appendTo('body')
 				.hide()
-				.css({
-					position : 'absolute',
-					overflow: 'auto',
-					transitionDuration: duration,
-					transitionDelay: 0,
-					transitionTimingFunction: 'liner',
-					left: '0',
-					zIndex: '9999'
-				})
 				;
 
 			view
 				.appendTo(wrap)
-				.addClass('sovHide')
 				.width('100%')
 				;
 
 			var body = $("body");
 			wrap.bind('transitionend webkitTransitionEnd', function(){
-				view.toggleClass('sovHide sovShow sovAnimating');
-				if(view.hasClass('sovHide')){
+				console.log('trasend');
+				wrap.toggleClass('sovHide sovShow');
+				if(wrap.hasClass('sovHide')){
+					wrap.removeClass('sovTransition');
+					wrap.addClass('sovNoTransition');
+					enableScroll();
 					wrap.hide();
-				} else {
-					op.body.hide();
 				}
+
+				wrap.removeClass('sovAnimating');
 			});
 
 			view.data('slideOverView', {
 				toggle: function(){
-					if(view.hasClass('sovAnimating')){
+
+					if(wrap.hasClass('sovAnimating')){
 						return;
 					}
+
+					disableScroll();
 
 					var windowRect = Rect.window.rect();
 
@@ -50,21 +65,22 @@ $.fn.extend({
 						.width(windowRect.size.width)
 						.height(windowRect.size.height);
 
-					view.addClass('sovAnimating');
+					wrap.addClass('sovAnimating');
 
-					var startY = windowRect.size.height + 1;
+					if(wrap.hasClass('sovHide')){
+						wrap.show();
+						var startOffset = {top: windowRect.offset.top + windowRect.size.height + 1, left:0};
+						wrap.offset(startOffset);
 
-					if(view.hasClass('sovHide')){
-						// wrap.css('transition-duration', 0);
 						setTimeout(function(){
-							wrap.show();
-							wrap.css('top', startY + 'px');
+							wrap.removeClass('sovNoTransition');
+							wrap.addClass('sovTransition');
 							setTimeout(function(){
-								wrap.css('transform', 'translateY(-'+ startY +'px)');
-							}, 100);
-						}, 0);
+								var targetY = windowRect.size.height + 1;
+								wrap.css('transform', 'translateY(-'+ targetY +'px)');
+							}, 0);
+						}, 100);
 					} else {
-						op.body.show();
 						wrap.css('transform', 'none');
 					}
 				}
